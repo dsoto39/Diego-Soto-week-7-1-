0d920d4b508660cadd2b7cf64e388ee06a361642
Diego Soto <113551815+dsoto39@users.noreply.github.com>
2022-10-03 19:55:23 +0000

    Even Odd

--- loops.py
+++ loops.py
@@ -1,4 +1,7 @@
-def loops():
+def loop():
+  # a = 999999999999999999999999999999999999999999999999999999999 
+  # while a > 10:
+  #   print(a)
   ###################################loops intro######################################
   # queue videos
   #what is iteration?
@@ -11,7 +14,8 @@ def loops():
   # For example: "Hello Norville"
   
   students = ["Norville", "Fred", "Velma", "Daphne"]
-  
+  for students in students:
+    print("Hello " + students)
   
   
   
@@ -24,12 +28,19 @@ def loops():
   
   
   
-  
   # For Loops Practice #3
   # Given the following list of numbers, perform the sum of all even and odd* numbers separately in the variables sum_even and sum_odd respectively:
   
   list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
-  
+  sum_even = 0
+  sum_odd = 0
+  for number in list_numbers:
+    if number %2 ==0:
+      sum_even = sum_even + number
+      print(f"evens are {sum_even}")
+    else:
+      sum_odd = sum_odd + number
+      print(f"evens are {sum_odd}")
   # *Recall from previous days: the modulus (or remainder) of a number divided by 2 is zero when said value is even, and 1 when it is odd
   
   # num % 2 == 0 (even values)
