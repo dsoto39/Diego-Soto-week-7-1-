d4beec830479343279c8ab49590c928a9d644a52
Diego Soto <113551815+dsoto39@users.noreply.github.com>
2022-10-03 19:22:33 +0000

    Import in main

--- while_loops.py
+++ while_loops.py
@@ -1,42 +1,42 @@
+def while_loops():
 
-
-########################################loops############################################################
-# while Loops
-# The while statement in Python is one of most general ways to perform iteration. A while statement will repeatedly execute a single statement or group of statements as long as the condition is true. The reason it is called a 'loop' is because the code statements are looped through over and over again until the condition is no longer met.
-
-# The general format of a while loop is:
-
-# while condition:
-#     code statements
-# else:
-#     final code statements
-
-# so a while loop is basically a way for python to loop through code several times until a condtion is met
-# Let’s look at a few simple while loops in action.
-#queue video about loops
-# i = 1
-# while i < 10:
-#   print(i)
-#   i = i + 1
-#   # or i += 1
-
-# print("done with loop")
-
-
-# we can use a while loop to continually ask the user to guess a word until they guess it correctly
-# secret_word ="giraffe"
-# guess= ""
-
-# while guess != secret_word:
-#   guess = input("enter a guess: ")
-
-# print("you win")
-
-
-# While Loops Practice #1
-# Create a While Loop that prints the numbers 10 to 0 on the screen, one at a time.
-
-number = 10
+  ########################################loops############################################################
+  # while Loops
+  # The while statement in Python is one of most general ways to perform iteration. A while statement will repeatedly execute a single statement or group of statements as long as the condition is true. The reason it is called a 'loop' is because the code statements are looped through over and over again until the condition is no longer met.
+  
+  # The general format of a while loop is:
+  
+  # while condition:
+  #     code statements
+  # else:
+  #     final code statements
+  
+  # so a while loop is basically a way for python to loop through code several times until a condtion is met
+  # Let’s look at a few simple while loops in action.
+  #queue video about loops
+  # i = 1
+  # while i < 10:
+  #   print(i)
+  #   i = i + 1
+  #   # or i += 1
+  
+  # print("done with loop")
+  
+  
+  # we can use a while loop to continually ask the user to guess a word until they guess it correctly
+  # secret_word ="giraffe"
+  # guess= ""
+  
+  # while guess != secret_word:
+  #   guess = input("enter a guess: ")
+  
+  # print("you win")
+  
+  
+  # While Loops Practice #1
+  # Create a While Loop that prints the numbers 10 to 0 on the screen, one at a time.
+  
+  number = 10
 
 
 
